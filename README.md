389a08acba5d1129f49f5703573296a92fa6762e
Ashish Gaurav <ashish.g308@gmail.com>
2023-10-22 15:13:19 -0700

    Added code for backend state

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Demo for CMPE 279 Project - Create a EC2 instance based web server which display a message over port 8080
 
-## This branch stores the state file locally.
+## This branch stores the state file externally - using S3 & DynamoDB.
--- main.tf
+++ main.tf
@@ -1,31 +1,42 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Demo for CMPE 279 Project!" > index.html
+              echo "Demo for CMPE 279 Project with external tf state storage! - local State!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 
   user_data_replace_on_change = true
-  vpc_security_group_ids      = [aws_security_group.instance.id]
+  vpc_security_group_ids      = [aws_security_group.sg1.id]
   tags = {
-    Name = "terraform-example"
+    Name = "cmpe279-project-demo-2"
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+resource "aws_security_group" "sg1" {
+  name = "cmpe279-demo-2-sg"
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "cmpe279-demo-tf-state"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "tf-cmpe279-project-state-locks"
+    encrypt        = true
   }
 }
